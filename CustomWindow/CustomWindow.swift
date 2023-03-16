6d0fcb283e8f44811228cbe3095f6eacda0b2920
kabikira <kabikirar2@protonmail.com>
2023-03-16 20:48:40 +0900

    first commit

---
+++ CustomWindow/CustomWindow.swift
@@ -0,0 +1,20 @@
+//
+//  CustomWindow.swift
+//  CustomWindow
+//
+//  Created by koala panda on 2023/03/16.
+//
+
+import UIKit
+
+
+class CustomWindow {
+    private var window: UIWindow?
+    
+    func showRoot(window: UIWindow?) {
+        let vc = ViewController()
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
+        self.window = window
+    }
+}
---
+++ CustomWindow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomWindow
+//
+//  Created by koala panda on 2023/03/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
